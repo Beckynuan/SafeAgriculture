a71c7702bfdf92d7bfde273157948428732a752f
Alfred Song <alfredsong@Alfreds-MacBook-Air.local>
2021-04-14 14:41:49 +1000

    add a new project

---
+++ Image/pic/app/src/main/java/com/test/pic/MainActivity.java
@@ -0,0 +1,23 @@
+package com.test.pic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageUrl);
+        Picasso.get().load("https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80").into(imageView);
+
+    }
+}
